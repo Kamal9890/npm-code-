703a53dc9c8bd79f32ae942ebc473f49bc822228
Kamal Pandey <kamalpandey9890@gmail.com>
2024-06-17 13:23:05 +0530

    first commit

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -10,7 +10,7 @@ const blogPosts = [
     title: 'PLAYING WITH PATTERNS',
     date: 'Apr 21, 2023',
     readTime: '1 min',
-    image: './assets/design for life', // replace with actual image path
+    image: 'src/assets/design for life.jpg', // replace with actual image path
     description: 'Create a blog post blurb that summarizes your post in a few short, punchy sentences and entices your audience to continue reading...',
   },
   {
@@ -18,7 +18,7 @@ const blogPosts = [
     title: 'NATALIA\'S APARTMENT MAKEOVER',
     date: 'Apr 21, 2023',
     readTime: '2 min',
-    image: 'path-to-image-2.jpg', // replace with actual image path
+    image: 'src/assets/design for life.jpg', // replace with actual image path
     description: 'Create a blog post blurb that summarizes your post in a few short, punchy sentences and entices your audience to continue reading...',
   },
 ];
